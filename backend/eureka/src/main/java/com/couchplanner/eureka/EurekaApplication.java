9faadf3586ffd73adc5b248d1e8eab79c0ead57a
LordDeSith <45897419+LordDeSith@users.noreply.github.com>
2020-12-23 17:59:32 +0100

    renaming and added docker

---
+++ backend/eureka/src/main/java/com/couchplanner/eureka/EurekaApplication.java
@@ -0,0 +1,16 @@
+package com.couchplanner.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.http.codec.ServerCodecConfigurer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
---
+++ backend/matching/src/test/java/com/couchplanner/matching/MatchingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.couchplanner.matching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
